88443544fb957692f2caaf3cc6c0748d5f0cfb5d
LaerteNF <laertenf@gmail.com>
2022-01-06 11:45:38 -0300

    ajuste interceptor

--- src/Service.ts
+++ src/Service.ts
@@ -1,25 +1,33 @@
-import axios, { AxiosResponse } from "axios"
-import handleAxiosResponseError from "./utils/handleAxiosResponseError"
-import handleAxiosResponseSuccess from "./utils/handleAxiosResponseSuccess"
+import axios, { AxiosRequestConfig, AxiosResponse } from "axios";
+import handleAxiosResponseError from "./utils/handleAxiosResponseError";
+import handleAxiosResponseSuccess from "./utils/handleAxiosResponseSuccess";
 
-const Http = axios.create()
+const Http = axios.create();
 
 class Service {
+  protected static Http = Http;
 
-    protected static Http = Http
+  protected static getData = getData;
 
-    protected static getData = getData
+  public static setRequestInterceptors(
+    onFulfilled: (
+      request: AxiosRequestConfig
+    ) => AxiosRequestConfig | Promise<AxiosRequestConfig>,
+    onRejected: (error: any) => any
+  ) {
+    Http.interceptors.request.use(onFulfilled, onRejected);
+  }
 }
 
 function getData<T>(res: AxiosResponse<T>) {
-  return res.data
+  return res.data;
 }
 
-Http.defaults.baseURL = 'http://localhost:8080'
+Http.defaults.baseURL = "http://localhost:8080";
 
 Http.interceptors.response.use(
   handleAxiosResponseSuccess,
   handleAxiosResponseError
-)
+);
 
-export default Service
+export default Service;
